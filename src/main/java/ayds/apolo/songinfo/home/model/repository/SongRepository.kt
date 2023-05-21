17a346c8a29338a55ddac17bda6a397dcd89a809
Joaquin Garcia Diotto <joaquinmichu@hotmail.com>
2023-05-21 19:25:44 -0300

    Respuestas

--- src/main/java/ayds/apolo/songinfo/home/model/repository/SongRepository.kt
+++ src/main/java/ayds/apolo/songinfo/home/model/repository/SongRepository.kt
@@ -4,6 +4,7 @@ import ayds.apolo.songinfo.home.model.entities.EmptySong
 import ayds.apolo.songinfo.home.model.entities.SearchResult
 import ayds.apolo.songinfo.home.model.entities.SpotifySong
 import ayds.apolo.songinfo.home.model.repository.external.spotify.SpotifyModule
+import ayds.apolo.songinfo.home.model.repository.local.spotify.SpotifyLocalStorage
 import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.ResultSetToSpotifySongMapperImpl
 import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.SpotifySqlDBImpl
 import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.SpotifySqlQueriesImpl
@@ -14,56 +15,61 @@ import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.IOException
 
+interface SongRepository {
 
-class SongRepository {
+    fun getSongByTerm(term: String): SearchResult
 
-    internal val sls = SpotifySqlDBImpl(
-        SpotifySqlQueriesImpl(), ResultSetToSpotifySongMapperImpl()
-    )
-    val sts = SpotifyModule.spotifyTrackService
+}
+internal class SongRepositoryImpl : SongRepository {
 
-    val theCache = mutableMapOf<String, SpotifySong>()
-
-    ///// Wiki
-    var retrofit: Retrofit? = Retrofit.Builder()
+    private val spotifyLocalStorage : SpotifyLocalStorage = SpotifySqlDBImpl(SpotifySqlQueriesImpl(), ResultSetToSpotifySongMapperImpl())
+    private val spotifyTrackService = SpotifyModule.spotifyTrackService
+    private val cache = mutableMapOf<String, SpotifySong>()
+    private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://en.wikipedia.org/w/")
         .addConverterFactory(ScalarsConverterFactory.create())
         .build()
+    private var wikipediaAPI: WikipediaAPI = retrofit.create(WikipediaAPI::class.java)
 
-    var wikipediaAPI = retrofit!!.create(WikipediaAPI::class.java)
-    //// end wiki
+    override fun getSongByTerm(term: String): SearchResult {
+        var spotifySong: SpotifySong?
 
-    fun getSongByTerm(term: String): SearchResult {
-        var s: SpotifySong?
+        spotifySong = getSongFromCache(term)
+        if (spotifySong != null)
+            return spotifySong
 
-        // check in the cache
-        s = theCache[term]
-        if (s != null) {
-            s.isCacheStored = true
-            return s
+        spotifySong = getSongFromLocalStorage(term)
+        if (spotifySong != null) {
+            storeSpotifySongInCache(term, spotifySong)
+            return spotifySong
         }
 
-        // check in the DB
-        s = sls.getSongByTerm(term)
-        if (s != null) {
-            s.isLocallyStored = true
-            // update the cache
-            theCache[term] = s
-            return s
+        spotifySong = getSongFromTrackService(term)
+        if (spotifySong != null) {
+            storeSpotifySongInLocalStorage(term, spotifySong)
+            return spotifySong
         }
 
-        // the service
-        s = sts.getSong(term)
-        if (s != null) {
-            sls.insertSong(term, s)
-            return s
-        }
+        spotifySong = getSongFromWikipediaAPI(term)
+        return spotifySong ?: EmptySong
+    }
 
-        /////// Last chance, get anything from the wiki
+    private fun getSongFromCache(term: String): SpotifySong? {
+        return cache[term]
+    }
+
+    private fun getSongFromLocalStorage(term: String): SpotifySong? {
+        return spotifyLocalStorage.getSongByTerm(term)
+    }
+
+    private fun getSongFromTrackService(term: String): SpotifySong? {
+        return spotifyTrackService.getSong(term)
+    }
+
+    private fun getSongFromWikipediaAPI(term: String): SpotifySong? {
         val callResponse: Response<String>
         try {
-            callResponse = wikipediaAPI.getInfo(term).execute()
-            System.out.println("JSON " + callResponse.body())
+            callResponse = getCallResponseFromWikipediaAPI(term)
             val gson = Gson()
             val jobj: JsonObject = gson.fromJson(callResponse.body(), JsonObject::class.java)
             val query = jobj["query"].asJsonObject
@@ -75,7 +81,21 @@ class SongRepository {
         } catch (e1: IOException) {
             e1.printStackTrace()
         }
-
-        return EmptySong
+        return null
     }
+
+    private fun storeSpotifySongInCache(term: String, spotifySong: SpotifySong){
+        cache[term] = spotifySong
+        spotifySong.isCacheStored = true
+    }
+
+    private fun storeSpotifySongInLocalStorage(term: String, spotifySong: SpotifySong){
+        spotifyLocalStorage.insertSong(term, spotifySong)
+        spotifySong.isLocallyStored = true
+    }
+
+    private fun getCallResponseFromWikipediaAPI(term: String): Response<String> {
+        return wikipediaAPI.getInfo(term).execute()
+    }
+
 }
